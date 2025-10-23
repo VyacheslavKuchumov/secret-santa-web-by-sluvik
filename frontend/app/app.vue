c355a576bb4ddb267e174e19b017db3a3d4c72e6
VyacheslavKuchumov <87779022+VyacheslavKuchumov@users.noreply.github.com>
2025-10-23 14:59:48 +0500

    init project

---
+++ frontend/app/app.vue
@@ -0,0 +1,13 @@
+<!-- app.vue -->
+<template>
+  <UApp>
+      <NavBar />
+    <UContainer class="flex items-center justify-center">
+      <NuxtPage class="mt-15" />
+    </UContainer>
+  </UApp>
+</template>
+
+<script setup lang="ts">
+
+</script>
